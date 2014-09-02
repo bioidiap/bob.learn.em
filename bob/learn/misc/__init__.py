519a4296429ed51c5b3d64e07e5244c2868646ca
Manuel Guenther <manuel.guenther@idiap.ch>
2014-09-02 17:56:21 +0200

    Added pure python function that got lost during porting.

--- bob/learn/misc/__init__.py
+++ bob/learn/misc/__init__.py
@@ -12,6 +12,22 @@ from ._old_library import *
 from . import version
 from .version import module as __version__
 
+
+def ztnorm_same_value(vect_a, vect_b):
+  """Computes the matrix of boolean D for the ZT-norm, which indicates where
+     the client ids of the T-Norm models and Z-Norm samples match.
+
+     vect_a An (ordered) list of client_id corresponding to the T-Norm models
+     vect_b An (ordered) list of client_id corresponding to the Z-Norm impostor samples
+  """
+  import numpy
+  sameMatrix = numpy.ndarray((len(vect_a), len(vect_b)), 'bool')
+  for j in range(len(vect_a)):
+    for i in range(len(vect_b)):
+      sameMatrix[j, i] = (vect_a[j] == vect_b[i])
+  return sameMatrix
+
+
 def get_config():
   """Returns a string containing the configuration information.
   """
