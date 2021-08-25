1fd7568d2cb7c0a0be0573d1162c03ecea443497
Yannick DAYER <yannick.dayer@idiap.ch>
2021-08-25 12:35:14 +0200

    Plot script to use python k-means

--- doc/plot/plot_kmeans.py
+++ doc/plot/plot_kmeans.py
@@ -1,6 +1,5 @@
 from bob.learn.em.cluster import KMeansMachine
 from bob.learn.em.cluster import KMeansTrainer
-from bob.learn.em import train
 import bob.db.iris
 import numpy
 import matplotlib.pyplot as plt
@@ -17,9 +16,11 @@ data = numpy.vstack((setosa, versicolor, virginica))
 
 # Training KMeans
 # 3 clusters with a feature dimensionality of 2
-machine = KMeansMachine(3, 2)
+machine = KMeansMachine(n_clusters=3)
 trainer = KMeansTrainer(init_method="k-means++")
-train(trainer, machine, data, max_iterations=200, convergence_threshold=1e-5)
+machine.fit(data, trainer=trainer)
+
+predictions = machine.predict(data)
 
 # Plotting
 figure, ax = plt.subplots()
@@ -29,8 +30,8 @@ plt.scatter(versicolor[:, 0],
             versicolor[:, 1], c="goldenrod", label="versicolor")
 plt.scatter(virginica[:, 0],
             virginica[:, 1], c="dimgrey", label="virginica")
-plt.scatter(machine.means[:, 0],
-            machine.means[:, 1], c="blue", marker="x", label="centroids",
+plt.scatter(machine.centroids_[:, 0],
+            machine.centroids_[:, 1], c="blue", marker="x", label="centroids",
             s=60)
 plt.legend()
 plt.xticks([], [])
@@ -38,4 +39,3 @@ plt.yticks([], [])
 ax.set_xlabel("Sepal length")
 ax.set_ylabel("Petal width")
 plt.tight_layout()
-plt.savefig("iris_kmeans.png")
