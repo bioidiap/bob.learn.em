53faf17fea1f948bd6c31842193123573651b705
Yannick DAYER <yannick.dayer@idiap.ch>
2021-09-07 15:07:21 +0200

    [doc] Plot code example of GMM

--- doc/plot/plot_ML.py
+++ doc/plot/plot_ML.py
@@ -1,7 +1,15 @@
-import bob.learn.em
+from bob.learn.em.mixture import MLGMMTrainer
+from bob.learn.em.mixture import GMMMachine
+from bob.learn.em.mixture import Gaussian
 import bob.db.iris
 import numpy
 import matplotlib.pyplot as plt
+
+from matplotlib.patches import Ellipse
+
+import logging
+logger = logging.getLogger("bob.learn.em")
+logger.setLevel("DEBUG")
 
 data_per_class = bob.db.iris.data()
 setosa = numpy.column_stack(
@@ -14,24 +22,55 @@ virginica = numpy.column_stack(
 data = numpy.vstack((setosa, versicolor, virginica))
 
 # Two clusters with a feature dimensionality of 3
-machine = bob.learn.em.GMMMachine(3, 2)
-trainer = bob.learn.em.ML_GMMTrainer(True, True, True)
-machine.means = numpy.array([[5, 3], [4, 2], [7, 3.]])
-bob.learn.em.train(trainer, machine, data, max_iterations=200,
-                   convergence_threshold=1e-5)  # Train the KMeansMachine
+machine = GMMMachine(3, convergence_threshold=1e-5)
 
-figure, ax = plt.subplots()
-plt.scatter(setosa[:, 0], setosa[:, 1], c="darkcyan", label="setosa")
-plt.scatter(versicolor[:, 0], versicolor[:, 1],
-            c="goldenrod", label="versicolor")
-plt.scatter(virginica[:, 0], virginica[:, 1],
-            c="dimgrey", label="virginica")
-plt.scatter(machine.means[:, 0],
-            machine.means[:, 1], c="blue", marker="x", label="centroids", s=60)
-plt.legend()
-plt.xticks([], [])
-plt.yticks([], [])
-ax.set_xlabel("Sepal length")
-ax.set_ylabel("Petal width")
-plt.tight_layout()
-plt.show()
+init_means = numpy.array([[5, 3], [4, 2], [7, 3]], dtype=float)
+gaussians = numpy.array([Gaussian(m) for m in init_means])
+trainer = MLGMMTrainer(
+    init_method=gaussians,
+    update_means=True,
+    update_variances=True,
+    update_weights=True,
+)
+
+trainer.initialize(machine, data)
+repeat = 8
+for step in range(10):
+    print(f"Step {step*repeat} through {step*repeat+repeat} out of {10*repeat}.")
+    for r in range(repeat):
+        print(f"Step {step*repeat+r}")
+        machine = machine.fit_partial(data, trainer=trainer)
+
+    figure, ax = plt.subplots()
+    ax.scatter(setosa[:, 0], setosa[:, 1], c="darkcyan", label="setosa")
+    ax.scatter(versicolor[:, 0], versicolor[:, 1],
+                c="goldenrod", label="versicolor")
+    ax.scatter(virginica[:, 0], virginica[:, 1],
+                c="dimgrey", label="virginica")
+    ax.scatter(
+        machine.gaussians_["mean"][:, 0],
+        machine.gaussians_["mean"][:, 1],
+        c="blue", marker="x", label="centroids", s=60
+    )
+
+    for g in machine.gaussians_:
+        covariance = numpy.diag(g["variance"])
+        radius = numpy.sqrt(5.991)
+        eigvals, eigvecs = numpy.linalg.eig(covariance)
+        axis = numpy.sqrt(eigvals) * radius
+        slope = eigvecs[1][0] / eigvecs[1][1]
+        angle = 180.0 * numpy.arctan(slope) / numpy.pi
+
+        e1 = Ellipse(
+            g["mean"], axis[0], axis[1],
+            angle=angle, linewidth=1, fill=False, zorder=2
+        )
+        ax.add_patch(e1)
+
+    plt.legend()
+    plt.xticks([], [])
+    plt.yticks([], [])
+    ax.set_xlabel("Sepal length")
+    ax.set_ylabel("Petal width")
+    plt.tight_layout()
+    plt.show()
