a736f9db2cd740cd5cdd622c96ac0379d2df771b
Yannick DAYER <yannick.dayer@idiap.ch>
2022-06-01 21:19:00 +0200

    Finalized IVector e-step

--- bob/learn/em/ivector.py
+++ bob/learn/em/ivector.py
@@ -2,7 +2,9 @@
 # @author: Yannick Dayer <yannick.dayer@idiap.ch>
 # @date: Fri 06 May 2022 14:18:25 UTC+02
 
-from typing import List
+import logging
+
+from typing import List, Tuple
 
 import numpy as np
 
@@ -10,33 +12,65 @@ from sklearn.base import BaseEstimator
 
 from bob.learn.em import GMMMachine, GMMStats
 
+logger = logging.getLogger("__name__")
+
 
 class IVectorStats:
-    """This class is a container for the statistics of an IVectorMachine."""
-
-    def __init__(self, dim_c, dim_d, dim_t) -> None:
+    def __init__(self, dim_c, dim_d, dim_t):
         self.dim_c = dim_c
         self.dim_d = dim_d
         self.dim_t = dim_t
+
+        # Accumulator storage variables
+
         # nij sigma wij2: shape = (c,t,t)
-        self.nij_sigma_wij2 = np.zeros(
+        self.acc_nij_sigma_wij2 = np.zeros(
             shape=(self.dim_c, self.dim_t, self.dim_t), dtype=float
         )
         # fnorm sigma wij: shape = (c,d,t)
-        self.fnorm_sigma_wij = np.zeros(
+        self.acc_fnorm_sigma_wij = np.zeros(
             shape=(self.dim_c, self.dim_d, self.dim_t), dtype=float
         )
-        self.snorm = np.zeros(
+        # Snormij (used only when updating sigma)
+        self.acc_snormij = np.zeros(
             shape=(
                 self.dim_c,
                 self.dim_d,
             ),
             dtype=float,
         )
-        self.n = np.zeros(shape=(self.dim_c,), dtype=float)
+        # Nij (used only when updating sigma)
+        self.acc_nij = np.zeros(shape=(self.dim_c,), dtype=float)
+
+    @property
+    def shape(self) -> Tuple[int, int, int]:
+        return (self.dim_c, self.dim_d, self.dim_t)
+
+    def __add__(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Cannot add stats of different shapes")
+        result = IVectorStats(self.dim_c, self.dim_d, self.dim_t)
+        result.acc_nij_sigma_wij2 = (
+            self.acc_nij_sigma_wij2 + other.acc_nij_sigma_wij2
+        )
+        result.acc_fnorm_sigma_wij = (
+            self.acc_fnorm_sigma_wij + other.acc_fnorm_sigma_wij
+        )
+        result.acc_snormij = self.acc_snormij + other.acc_snormij
+        result.acc_nij = self.acc_nij + other.acc_nij
+        return result
+
+    def __iadd__(self, other):
+        if self.shape != other.shape:
+            raise ValueError("Cannot add stats of different shapes")
+        self.acc_nij_sigma_wij2 += other.acc_nij_sigma_wij2
+        self.acc_fnorm_sigma_wij += other.acc_fnorm_sigma_wij
+        self.acc_snormij += other.acc_snormij
+        self.acc_nij += other.acc_nij
+        return self
 
 
-def compute_tct_sigmac_inv(T, sigma):
+def compute_tct_sigmac_inv(T: np.ndarray, sigma: np.ndarray) -> np.ndarray:
     """Computes T_{c}^{T}.sigma_{c}^{-1}"""
     # C++ to port to python:
     #
@@ -48,20 +82,25 @@ def compute_tct_sigmac_inv(T, sigma):
     # blitz::Array<double,1> sigma_c = m_sigma(blitz::Range(c*D,(c+1)*D-1));
     # Tct_sigmacInv = Tct(i,j) / sigma_c(j);
     # }
-    # python version:
+
+    # Python version:
+    # Tct_sigmacInv = np.zeros(shape=(c,d,t))
     # for c in range(dim_c):
     #     Tc = T[c]
     #     Tct = Tc.transpose(1, 0)
     #     sigma_c = sigma[c]
-    #     Tct_sigmacInv = T[c].t / sigma[c]
+    #     Tct_sigmacInv[c] = T[c].t / sigma[c]
 
     # Vectorized version:
 
-    Tct_sigmacInv = T.transpose(0, 2, 1) / sigma[None]
+    # T.T (c,t,d) / sigma (c,1,d)
+    Tct_sigmacInv = T.transpose(0, 2, 1) / sigma[:, None, :]
+
+    # Tt_sigma_inv (c,t,d)
     return Tct_sigmacInv
 
 
-def compute_tct_sigmac_inv_tc(self, stats: GMMStats):
+def compute_tct_sigmac_inv_tc(T: np.ndarray, sigma: np.ndarray) -> np.ndarray:
     """Computes T_{c}^{T}.sigma_{c}^{-1}.T_{c}"""
 
     # for (int c=0; c<C; ++c)
@@ -71,16 +110,22 @@ def compute_tct_sigmac_inv_tc(self, stats: GMMStats):
     # blitz::Array<double,2> Tct_sigmacInv_Tc = m_cache_Tct_sigmacInv_Tc(c, rall, rall);
     # bob::math::prod(Tct_sigmacInv, Tc, Tct_sigmacInv_Tc);
     # }
-    # python version:
 
-    pass
+    Tct_sigmacInv_Tc = np.zeros(shape=(T.shape[0], T.shape[-1], T.shape[-1]))
+
+    # Python version:
+    tct_sigmac_inv = compute_tct_sigmac_inv(T, sigma)
+    for c in range(T.shape[0]):  # TODO Vectorize
+        # (c,t,t) = (c,t,d) @ (c,d,t)
+        Tct_sigmacInv_Tc[c] = tct_sigmac_inv[c] @ T[c]
+
+    # Output: shape (c,t,t)
+    return Tct_sigmacInv_Tc
 
 
-def compute_tt_sigma_inv_fnorm(self, stats: GMMStats) -> np.ndarray:
-    """Computes \f$(Id + \\sum_{c=1}^{C} N_{i,j,c} T^{T} \\Sigma_{c}^{-1} T)\f$
-
-    Returns an array of shape (?,?) TODO dims!
-    """
+def compute_id_tt_sigma_inv_t(
+    stats: GMMStats, T: np.ndarray, sigma: np.ndarray
+) -> np.ndarray:
     # void bob::learn::em::IVectorMachine::computeIdTtSigmaInvT(
     #   const bob::learn::em::GMMStats& gs, blitz::Array<double,2>& output) const
     # {
@@ -89,10 +134,25 @@ def compute_tt_sigma_inv_fnorm(self, stats: GMMStats) -> np.ndarray:
     #   for (int c=0; c<(int)getNGaussians(); ++c)
     #     output += gs.n(c) * m_cache_Tct_sigmacInv_Tc(c, rall, rall);
     # }
-    # output = np.eye((self.dim_t, self.dim_t))  # TODO dims!
-    # for c in range(self.dim_c):
-    #     output += stats.n[c] * Tct_sigmacInv_Tc[c]
-    pass
+    dim_t = T.shape[-1]
+    output = np.eye(dim_t, dim_t)
+    tct_sigmac_inv_tc = compute_tct_sigmac_inv_tc(T, sigma)
+    for c in range(stats.n.shape[0]):  # TODO Vectorize
+        # (t,t) += scalar * (c,t,t)
+        output += stats.n[c] * tct_sigmac_inv_tc[c]
+
+    # Output: (t,t)
+    return output
+
+
+def compute_tt_sigma_inv_fnorm(
+    ubm_means: np.ndarray, stats: GMMStats, T: np.ndarray, sigma: np.ndarray
+) -> np.ndarray:
+    """Computes \f$(Id + \\sum_{c=1}^{C} N_{i,j,c} T^{T} \\Sigma_{c}^{-1} T)\f$
+
+    Returns an array of shape (t,)
+    """
+
     # void bob::learn::em::IVectorMachine::computeTtSigmaInvFnorm(
     #   const bob::learn::em::GMMStats& gs, blitz::Array<double,1>& output) const
     # {
@@ -109,6 +169,20 @@ def compute_tt_sigma_inv_fnorm(self, stats: GMMStats) -> np.ndarray:
     #   }
     # }
 
+    output = np.zeros(shape=T.shape[-1])
+    tct_sigmac_inv = compute_tct_sigmac_inv(T, sigma)  # (c,t,d)
+    fnorm = stats.sum_px - stats.n[:, None] * ubm_means  # (c,d)
+    for c in range(T.shape[0]):  # TODO Vectorize
+        # (t,) += (c,t,d) @ (c,d)
+        output += tct_sigmac_inv[c] @ fnorm[c]
+
+    # Output: shape (t,)
+    return output
+
+
+def forward(
+    ubm_means: np.ndarray, stats: GMMStats, T: np.ndarray, sigma: np.ndarray
+):
     # void bob::learn::em::IVectorMachine::forward_(const bob::learn::em::GMMStats& gs,
     #   blitz::Array<double,1>& ivector) const
     # {
@@ -121,6 +195,10 @@ def compute_tt_sigma_inv_fnorm(self, stats: GMMStats) -> np.ndarray:
     #   // Solves m_tmp_tt.ivector = m_tmp_t1
     #   bob::math::linsolve(m_tmp_tt, m_tmp_t1, ivector);
     # }
+    return np.linalg.solve(
+        compute_id_tt_sigma_inv_t(stats, T, sigma),
+        compute_tt_sigma_inv_fnorm(ubm_means, stats, T, sigma),
+    )
 
 
 class IVectorMachine(BaseEstimator):
@@ -128,7 +206,7 @@ class IVectorMachine(BaseEstimator):
 
     Dimensions:
         - dim_c: number of Gaussians
-        - dim_d: numer of features
+        - dim_d: number of features
         - dim_t: dimension of the i-vector
     Attributes:
         T (c,d,t): The total variability matrix \f$T\f$
@@ -142,7 +220,7 @@ class IVectorMachine(BaseEstimator):
         convergence_threshold: float = 1e-5,
         max_iterations: int = 25,
         update_sigma: bool = True,
-        **kwargs
+        **kwargs,
     ) -> None:
         """Initializes the IVectorMachine object.
 
@@ -162,24 +240,19 @@ class IVectorMachine(BaseEstimator):
         self.update_sigma = update_sigma
         # TODO: add params
         # self.compute_likelihood = compute_likelihood
-        # self.sigma_update = sigma_update
         # self.variance_floor = variance_floor
-        self.dim_d = self.ubm.n_gaussians
-        self.dim_c = self.ubm.means.shape[-1]
+        self.dim_c = self.ubm.n_gaussians
+        self.dim_d = self.ubm.means.shape[-1]
 
-    def initialize(self, features: np.ndarray) -> None:
-        """Initializes the I-Vector parameters at fit time."""
-        # TODO implement
-        # self.
-        # self.dim_d =
-        # self.dim_c =
-        pass
+        self.T = np.zeros(shape=(self.dim_c, self.dim_d, self.dim_t))
+        self.sigma = np.zeros(shape=(self.dim_c, self.dim_d))
 
     def e_step(self, data: List[GMMStats]) -> IVectorStats:
         """Computes the expectation step of the e-m algorithm."""
         n_samples = len(data)
 
         stats = IVectorStats(self.dim_c, self.dim_d, self.dim_t)
+        ubm_means = self.ubm.means
 
         for n in range(n_samples):
             Nij = data[n].n
@@ -187,8 +260,12 @@ class IVectorMachine(BaseEstimator):
             Sij = data[n].sum_pxx
 
             # Estimate latent variables
-            TtSigmaInv_Fnorm = self.compute_TtSigmaInvFnorm(data[n])
-            I_TtSigmaInvNT = self.compute_Id_TtSigmaInvT(data[n])
+            TtSigmaInv_Fnorm = compute_tt_sigma_inv_fnorm(
+                ubm_means, data[n], self.T, self.sigma
+            )  # self.compute_TtSigmaInvFnorm(data[n]) # shape: (t,)
+            I_TtSigmaInvNT = compute_id_tt_sigma_inv_t(
+                data[n], self.T, self.sigma
+            )  # self.compute_Id_TtSigmaInvT(data[n]), # shape: (t,t)
 
             Fnorm = np.zeros(
                 shape=(
@@ -206,67 +283,112 @@ class IVectorMachine(BaseEstimator):
             )
 
             # Latent variables
-            I_TtSigmaInvNT_inv = np.linalg.inv(I_TtSigmaInvNT)
-            E_w_ij = np.dot(I_TtSigmaInvNT_inv, TtSigmaInv_Fnorm)
-            E_w_ij2 = I_TtSigmaInvNT_inv + np.outer(E_w_ij, E_w_ij)
+            I_TtSigmaInvNT_inv = np.linalg.inv(I_TtSigmaInvNT)  # shape: (t,t)
+            sigma_w_ij = np.dot(
+                I_TtSigmaInvNT_inv, TtSigmaInv_Fnorm
+            )  # shape: (t,)
+            sigma_w_ij2 = I_TtSigmaInvNT_inv + np.outer(
+                sigma_w_ij, sigma_w_ij
+            )  # shape: (t,t)
 
             # Compute normalized statistics
-            for c in range(self.dim_c):
+            # for c in range(self.dim_c):  # TODO Vectorize
 
-                Fc = Fij[c, :]
-                Sc = Sij[c, :]
-                mc = self.means[c]
+            #     Fc = Fij[c, :]
+            #     Sc = Sij[c, :]
+            #     mc = self.ubm.means[c]
 
-                Fc_mc = Fc * mc
-                Nc_mc_mcT = Nij[c] * mc * mc
+            #     Fc_mc = Fc * mc
+            #     Nc_mc_mcT = Nij[c] * mc * mc
 
-                Fnorm[c] = Fc - Nij[c] * mc
-                Snorm[c] = Sc - (2 * Fc_mc) + Nc_mc_mcT
+            #     Fnorm[c] = Fc - Nij[c] * mc
+            #     Snorm[c] = Sc - (2 * Fc_mc) + Nc_mc_mcT
+
+            Fnorm = Fij - Nij[:, None] * self.ubm.means
+            Snorm = (
+                Sij
+                - (2 * Fij * self.ubm.means)
+                + (Nij[:, None] * self.ubm.means * self.ubm.means)
+            )
 
             # Do the accumulation for each component
-            stats.snorm += Snorm  # (dim_c, dim_d)
+            stats.acc_snormij += Snorm  # (dim_c, dim_d)
 
             for c in range(self.dim_c):
-                stats.nij_sigma_wij2[c] += Nij[c] * E_w_ij2  # (dim_t, dim_t)
-                stats.fnorm_sigma_wij[c] += np.outer(
-                    Fnorm[c], E_w_ij
-                )  # (dim_d, dim_t)
-                stats.n[c] += Nij[c]
+                stats.acc_nij_sigma_wij2 += (
+                    Nij[c] * sigma_w_ij2
+                )  # (dim_t, dim_t)
+            # stats.acc_nij_sigma_wij2 += Nij[:, None] * sigma_w_ij2  # (c, t, t) # TODO Not working
+            stats.acc_nij += Nij
+            # for c in range(self.dim_c):  # TODO Vectorize
+            #     stats.acc_fnorm_sigma_wij[c] += np.outer(
+            #         Fnorm[c], sigma_w_ij  # (c,d) x (t,)
+            #     )  # (dim_d, dim_t)
+            stats.acc_fnorm_sigma_wij += np.matmul(
+                Fnorm[:, :, None], sigma_w_ij[None, :]
+            )  # (c,d,t)
 
-        # TODO convert
-        pass
+        return stats
 
     def m_step(self, stats: IVectorStats) -> None:
         """Updates the Machine with the maximization step of the e-m algorithm."""
-        A = stats.nij_sigma_wij2
+        A = stats.acc_nij_sigma_wij2
 
-        T = np.zeros(
+        self.T = np.zeros(
             shape=(self.dim_c, self.dim_d, self.dim_t),
             dtype=np.float64,
         )
-        t_old = stats.t
         if self.update_sigma:
-            sigma = np.zeros(shape=stats.snorm.shape, dtype=np.float64)
-        for c in range(self.dim_c):
+            self.sigma = np.zeros(
+                shape=stats.acc_snormij.shape, dtype=np.float64
+            )
+        for c in range(self.dim_c):  # TODO Vectorize
             # T update
-            A = stats.nij_sigma_wij2[c].transpose()
-            B = stats.fnorm_sigma_wij[c].transpose()
+            A = stats.acc_nij_sigma_wij2[c].transpose()
+            B = stats.acc_fnorm_sigma_wij[c].transpose()
             if not A.any():  # if all A == 0
                 X = np.zeros(shape=(self.dim_t, self.dim_d), dtype=np.float64)
             else:
                 X = np.linalg.solve(A, B)
-            T[c, :] = X.transpose()
+            self.T[c, :] = X.transpose()
             # Sigma update
             if self.update_sigma:
-                _ = t_old  # TODO: remove
                 # t_old_c = t_old[c, :].transpose()
                 # warning: Use of the new T estimate! (toggle the two next line if you don't want that)
-                Fnorm_Ewij_Tt = np.diag(np.dot(stats.fnorm_sigma_wij[c], X))
+                Fnorm_sigma_w_ij_Tt = np.diag(
+                    np.dot(stats.acc_fnorm_sigma_wij[c], X)
+                )
                 # Fnorm_Ewij_Tt = np.diag(np.dot(stats.fnorm_sigma_wij[c], t_old_c))
-                sigma[c] = (stats.snorm[c] - Fnorm_Ewij_Tt) / stats.n[c]
-        pass
+                self.sigma[c] = (
+                    stats.acc_snormij[c] - Fnorm_sigma_w_ij_Tt
+                ) / stats.acc_nij[c]
 
     def fit(self, data: np.ndarray) -> "IVectorMachine":
-        """Trains the IVectorMachine."""
-        # TODO implement
+        """Trains the IVectorMachine.
+
+        Repeats the e-m steps until the convergence criterion is met or
+        ``max_iterations`` is reached.
+        """
+
+        t_old = 0
+        for step in range(self.max_iterations):
+            logger.debug(f"IVector step {step+1}.")
+            # E-step
+            stats = self.e_step(data)
+            # M-step
+            self.m_step(stats)
+            # Convergence
+            if step > 0:
+                if np.linalg.norm(stats.t - t_old) < self.convergence_threshold:
+                    logger.info(f"Converged after {step+1} steps.")
+                    logger.debug(
+                        f"t_diff: {stats.t - t_old} (Convergence threshold: {self.convergence_threshold})"
+                    )
+                    break
+            t_old = stats.t
+        else:
+            logger.info(f"Did not converge after {step+1} steps.")
         return self
+
+    def project(self, stats: GMMStats) -> IVectorStats:
+        return forward(self.ubm.means, stats, self.T, self.sigma)
